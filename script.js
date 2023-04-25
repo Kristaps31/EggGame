ce288f0a4451585669accbfcda284dd99cacb39a
Kristaps31 <kristaps.v@hotmail.co.uk>
2023-04-25 10:47:49 +0100

    contains code to check if player is colliding with obsticles

--- script.js
+++ script.js
@@ -42,9 +42,12 @@ window.addEventListener('load', function(){
                 this.speedX = 0;
                 this.speedY = 0;
             }
-            
             this.collisionX += this.speedX * this.speedModifier;
             this.collisionY += this.speedY * this.speedModifier;
+            this.game.obstacles.forEach(obstacle => {
+                 if (this.game.checkCollision(this, obstacle)){
+                    console.log("collision");
+                 }})
         }
     }
 
@@ -112,6 +115,13 @@ window.addEventListener('load', function(){
             this.player.draw(context);
             this.player.update();
             this.obstacles.forEach(obstacle => obstacle.draw(context))
+        }
+        checkCollision(a,b){
+            const dx = a.collisionX - b.collisionX;
+            const dy = a.collisionY - b.collisionY;
+            const distance = Math.hypot(dy, dx);
+            const sumOfRadii = a.collisionRadius + b.collisionRadius;
+            return (distance < sumOfRadii);
         }
         init(){
             let attempts = 0;
