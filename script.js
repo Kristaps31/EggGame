df9a12720e3cbebdc5e37ee579ebb0cd7436ca81
Kristaps31 <kristaps.v@hotmail.co.uk>
2023-05-03 15:03:11 +0100

    contains code to add egg pushing around the play area

--- script.js
+++ script.js
@@ -145,8 +145,8 @@ window.addEventListener('load', function(){
             this.spriteWidth = 110;
             this.width = this.spriteWidth;
             this.height = this.spriteHeight;
-            this.spriteX = this.collisionX - this.width * 0.5;
-            this.spriteY = this.collisionY - this.height * 0.5 - 30;
+            this.spriteX;
+            this.spriteY;
         }
         draw(context){
             context.drawImage(this.image, this.spriteX, this.spriteY);
@@ -159,6 +159,21 @@ window.addEventListener('load', function(){
                 context.restore();
                 context.stroke();
             }
+        }
+        update(){
+            this.spriteX = this.collisionX - this.width * 0.5;
+            this.spriteY = this.collisionY - this.height * 0.5 - 30;
+            let collisionObjects = [this.game.player, ...this.game.obstacles];
+            collisionObjects.forEach(object => {
+                let [collision, distance, sumOfRadii, dx, dy] = this.game.checkCollision(this, object);
+                if (collision){
+                    const unit_x = dx / distance;
+                    const unit_y = dy / distance;
+                    this.collisionX = object.collisionX + (sumOfRadii + 1) * unit_x;
+                    this.collisionY = object.collisionY + (sumOfRadii + 1) * unit_y;
+
+                }
+            });
         }
     }
 
@@ -209,7 +224,10 @@ window.addEventListener('load', function(){
             if (this.timer > this.interval){
                 context.clearRect(0, 0, this.width, this.height);
                 this.obstacles.forEach(obstacle => obstacle.draw(context));
-                this.eggs.forEach(egg => egg.draw(context));
+                this.eggs.forEach(egg => {
+                    egg.draw(context)
+                    egg.update();
+                });
                 this.player.draw(context);
                 this.player.update();
                 this.timer = 0;
